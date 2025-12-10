a2ce4a1435f615c30fbe72fe06316962aae179e3
RendiSinaga <rendysinagaa10@gmail.com>
2025-12-10 15:26:20 +0700

    perubahan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RolesSeeder::class,
             ColorSeeder::class,
             SubscriptionSeeder::class,
+            AdminSistemSeeder::class,
         ]);
     }
 }
