2fb0c77b780c381ddfe2bf08bb8c6890e23e3301
Naufal Fadhilah Ardhani <naufal201080@gmail.com>
2026-03-05 23:03:31 +0700

    Login via Google tidak perlu 2 kali lagi

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +13,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, SoftDeletes;
+    use HasUuids;
 
     public $incrementing = false;
     protected $keyType = 'string';
