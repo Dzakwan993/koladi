6c74971d5345467731b1fab90af2a431cba441e8
Naufal Fadhilah Ardhani <naufal201080@gmail.com>
2026-09-13 11:14:23 +0700

    fix: build fresh assets and expose documentSearch

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import Alpine from 'alpinejs';
 import documentSearch from './dokumen-script.js'
 
 window.Alpine = Alpine;
+window.documentSearch = documentSearch;
 
 // Register komponen supaya bisa dipanggil di Blade
 Alpine.data('documentSearch', documentSearch)
